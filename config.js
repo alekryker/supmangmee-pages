d868be5fe36333615d821618ca9a460193a5956d
Prin <prin@MacBook-Pro.local>
2026-08-21 15:05:09 +0700

    Use Cloud Run API

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.LOTTERY_API_URL = "https://script.google.com/macros/s/AKfycbyDnjGnxA1CtMD1TWxUCE8ThyQ8nxRG2kXJ7-bEkmCKfyJ3SsgghHP9Qj1OmyshyUI/exec";
+window.LOTTERY_API_URL = "https://supmangmee-api-49991855284.asia-southeast1.run.app/api";
